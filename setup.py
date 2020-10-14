8267612c27670df4a11995a23760d63f24a4670d
Lily Karim <lilyk@Lilys-MacBook-Pro.local>
2020-10-13 17:11:08 -0700

    init

---
+++ setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup(
+	name='sporecore',
+	version='0.0.1',
+	author='Lily Karim',
+	author_email='lilymaryam5@gmail.com',
+	description='2020 UC Davis iGEM Competition',
+	url='https://github.com/igemsoftware2020/UC-Davis',
+	packages=setuptools.find_packages(),
+	classifiers=[
+		'Programming Language :: Python :: 3',
+		'License :: OSI Approved :: MIT License',
+		'Operating System :: OS Independent',
+	],
+	python_requires='>=3.6',
+)
+
